5c57c159113f7e3187cc42b865c0391d0eaa4b28
cultab <rroarck@gmail.com>
2022-06-13 20:32:00 +0300

    add messages to setup_ssh

--- setup_ssh.sh
+++ setup_ssh.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
+echo "Creating git user.."
 # create user git with
 sudo useradd -U --uid 1337 --groups docker --create-home git
 
+echo "Generating ssh host key.."
 # create key for git user
 sudo -u git ssh-keygen -b 4096 -t rsa -f /home/git/.ssh/id_rsa -q -N "" -C "Gitea Host Key"
 
+echo "Adding generated host key to /home/git/.ssh/authorized_keys.."
 # edit authorized_keys
 sudo -u git cat /home/git/.ssh/id_rsa.pub | sudo -u git tee -a /home/git/.ssh/authorized_keys
 sudo -u git chmod 600 /home/git/.ssh/authorized_keys
 
+echo "Creating docker-shell.."
 # add docker login shell
 cat <<"EOF" | sudo tee /home/git/docker-shell
 #!/bin/sh
@@ -17,8 +21,8 @@ cat <<"EOF" | sudo tee /home/git/docker-shell
 
 EOF
 
+echo "Setting docker-shell as git user's login shell.."
 sudo chmod +x /home/git/docker-shell
 sudo usermod -s /home/git/docker-shell git
 
-
-
+echo "done!"
